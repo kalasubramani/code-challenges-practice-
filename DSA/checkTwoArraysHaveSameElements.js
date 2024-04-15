b9455c5daf6e2afd1c24309341c6bb433afc625b
KalaSubramani <kalasubramani@gmail.com>
2024-04-15 17:59:28 -0400

    checkTwoArrays

---
+++ DSA/checkTwoArraysHaveSameElements.js
@@ -0,0 +1,13 @@
+function checkTwoArraysHaveSameElements(){
+  // return JSON.stringify(arr1)===JSON.stringify(arr2);
+  const equalValues = (arr1.length === arr2.length) && arr1.every((value, index) => value === arr2[index]);  
+  return equalValues;
+  // return arr1===arr2;
+}
+
+const arr1=[3,4,5], arr2=[3,4,5], arr3=arr1;;
+
+
+console.log("checkTwoArraysHaveSameElements ", checkTwoArraysHaveSameElements(arr1,arr2))
+console.log("checkTwoArraysHaveSameElements same ref ", checkTwoArraysHaveSameElements(arr1,arr3))
+
