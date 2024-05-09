44235f22b57243c78ed6fa604720dd91d4538c3f
KalaSubramani <kalasubramani@gmail.com>
2024-05-09 13:49:01 -0400

    Ex 8,0 minsubArrayLen,findLongestSubString

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -1,7 +1,40 @@
+// Write a function called findLongestSubstring, which accepts a string and 
+// returns the length of the longest substring with all distinct characters.
 
+// findLongestSubstring('') // 0
+// findLongestSubstring('rithmschool') // 7
+// findLongestSubstring('thisisawesome') // 6
+// findLongestSubstring('thecatinthehat') // 7
+// findLongestSubstring('bbbbbb') // 1
+// findLongestSubstring('longestsubstring') // 8
+// findLongestSubstring('thisishowwedoit') // 6
+// Time Complexity - O(n)
 
 function findLongestSubstring(str){
+  //use two pointers i,j and a set
+  
+  //ifj<str.length, 
+      //check length of longest string, if len > longest string, replace longest string 
+      //clear curr string
+  //add j to a set
+    //move j = j+1 
+  //repeat until end of string
+  //return curr string
 
+  let i=0,j=0, currString = new Set(), longestSubString;
+
+  while( j<str.length){
+    if(currString.has(str[j])){ 
+      longestSubString = longestSubString?Math.max(currString.size,longestSubString):currString.size;
+      currString.clear();           
+    }
+    currString.add(str[j]); 
+    j++;
+  }
+  // //to get len of last sub string
+  longestSubString = Math.max(currString.size,longestSubString)
+return longestSubString;
 }
 
-console.log(findLongestSubstring());
+console.log(findLongestSubstring('rithmschool'));
+console.log(findLongestSubstring('thisishowwedoit'));
--- minSubArrayLen.js
+++ minSubArrayLen.js
@@ -6,46 +6,72 @@
 // This function should return the minimal length of a contiguous subarray 
 // of which the sum is greater than or equal to the integer passed to the function. 
 // If there isn't one, return 0 instead.
+// Time Complexity - O(n)
+// Space Complexity - O(1)
 
 function minSubArrayLen(arr,k){
-  //set minLength=0;
-  //find sum of first two ele, check if sum >=k
-  //if sum === k, 
+  //set minLength=0, i-0,j=0;
+  //find sum of first n ele, check if sum >=k
+  //until j and i < arr.length 
+    //if sum <k
+        //add next ele
+        // move j to next pos
+  //else if sum >= k, 
       //find length of subarray
-      //check if subArray len > minLength, store it in minLen
-  //if sum < k, add n+1 th ele and check sum >= k
+      //check if subArray len > minLength? replace minLen  
+      //remove first ele
+      //subract first ele from sum
+      //move i to next pos  
   //return minLength
 
-  let minLength, sum =0,subArr=[],subArrLen=2;
+  let minLength,i=0,j=0,sum =0,subArr=[],subArrLen=2;
   if(!arr){
     return 0;
   }
-
-  subArr.push(arr[0]);
-  subArr.push(arr[1]);
-  sum = arr[0]+arr[1];
-  if(sum>=k){
-    minLength=subArr.length;
-  }
-
-
-for(let i=2;i<arr.length;i++){  
-  sum = (sum - arr[i-subArrLen]) + arr[i];
-  subArr.shift();
-  subArr.push(arr[i]);
-  if(sum >= k){   
-    console.log("subArr ",subArr);
-    if(!minLength){
-      minLength = subArr.length;
-    }else{
-      minLength = subArr.length<minLength?subArr.length:minLength;     
-    }
-  }
-    
-}//for
+  while(i<arr.length && j<arr.length){  
+  if(sum<k){
+    sum = sum + arr[j];
+    subArr.push(arr[j]);
+    j++;//move j to next pos   
+  }else if(sum >= k){      
+    minLength = minLength ? Math.min(subArr.length,minLength) : subArr.length;
+     sum -=subArr.shift(); //remove first ele
+     i++;//move first pointer
+  }    
+  console.log("subArr ",subArr);
+}//while
 
 return minLength>0?minLength:0;
 }
 
+
+// function minSubArrayLen(nums, sum) {
+//   let total = 0;
+//   let start = 0;
+//   let end = 0;
+//   let minLen = Infinity;
+ 
+//   while (start < nums.length) {
+//     // if current window doesn't add up to the given sum then 
+// 		// move the window to right
+//     if(total < sum && end < nums.length){
+//       total += nums[end];
+// 			end++;
+//     }
+//     // if current window adds up to at least the sum given then
+// 		// we can shrink the window 
+//     else if(total >= sum){
+//       minLen = Math.min(minLen, end-start);
+// 			total -= nums[start];
+// 			start++;
+//     } 
+//     // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+//     else {
+//       break;
+//     }
+//   }
+ 
+//   return minLen === Infinity ? 0 : minLen;
+// }
 console.log(minSubArrayLen([2,3,1,2,4,3,6,1], 7));
 console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95));
