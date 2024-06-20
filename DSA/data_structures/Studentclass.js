1e9a0937f4343f776a0fe09cd98aa436469c9f96
KalaSubramani <kalasubramani@gmail.com>
2024-06-19 23:24:15 -0400

    datastr - linkedlist

--- DSA/data_structures/Studentclass.js
+++ DSA/data_structures/Studentclass.js
@@ -1,17 +1,21 @@
-class Studentclass{
+class Studentclass{ 
+   classCount=0; 
  constructor(firstName,lastName, year){
   this.firstName = firstName;
   this.lastName = lastName;
   this.grade = year;
   this.tardie = 0;
-  this.classCount = 0;
+  this.addToClassCount();
+ }
+ addToClassCount(){
+  classCount +=1;
  }
  markLate(){
   this.tardie++;
   console.log(`${this.firstName} is late ${this.tardie} days`); 
  }
  static classCount(){
-   return `CLASS Strenght ${this.classCount}`
+   return `CLASS Strenght ${classCount}`
  }
 }
 let newStudent = new Studentclass("pavi", "kalay", 3);
@@ -20,4 +24,4 @@ console.log(newStudent);
 newStudent.markLate();
 
 let classCount = Studentclass.classCount();
-console.log(classCount);
+console.log("classCount ",classCount);
