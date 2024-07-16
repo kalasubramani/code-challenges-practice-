a8f9a14a12ff2358a64e20635bd4ec333cf8ebf4
KalaSubramani <kalasubramani@gmail.com>
2024-07-16 16:49:14 -0400

    SLL - set,insert,remove, reverse

--- DSA/data_structures/singly_linked_list.js
+++ DSA/data_structures/singly_linked_list.js
@@ -114,10 +114,9 @@ class singly_linked_list {
     if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
     }
+    newNode.next = this.head;
+    this.head = newNode;
 
     this.length += 1;
     return this;
@@ -134,19 +133,112 @@ class singly_linked_list {
     if (this.length === 0) return undefined;
     if (index < 0 || index > this.length - 1) return undefined;
     let counter = 0, currentNode = this.head;
-    // if(index===0 && this.length===1) return this.head.val;
+
     while (counter <= index) {
-      if (counter === index) return currentNode.value;
+      if (counter === index) return currentNode;
       counter++;
       currentNode = currentNode.next;
     }
   }
 
   //update the value of a node at given index
-  set(index,value){
-    //if list.length ==0. then return null
-    //if index <0 or index >=list.length, return undefined
-    
+  set(index, value) {
+    //make use of get() to get the value of the node
+    //update the value of the node to new value
+    //return true
+
+    let currentNode = this.get(index);
+    if (currentNode) {
+      currentNode.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  //inserts a new node at a given index
+  insert(index,val){
+    //if index <0 and > length, return false
+    //create new node using the value
+    //if index is 0, use unshift()
+    //if index is equal to length, use push()
+    // use get() to get the node at index-1 --> previous node
+    // use get() to get the node at index+1 --> next node
+    // set previous.next as new node
+    //set new node.next as next node
+    // increment length
+    //return true
+
+    if(index<0 || index > this.length) return false;   
+    if(index===0) !!this.unshift(val); //!! makes it to return a boolean value
+    if(index===this.length) !!this.push(val);
+
+    let newNode = new Node(val);
+    let previous = this.get(index-1);
+    let next = previous.next;
+    console.log("next ",next);
+    previous.next=newNode;
+    newNode.next=next;
+
+    this.length++;
+    return true;
+  }
+
+  //removes a node at a specific index
+  remove(index){
+    //if index <0 or >length, return false
+    //if index ===0, call shift(), return true
+    //if index ===length-1 , call pop(), return true
+    //use get(index-1) to get previous node
+    //use get(index) to get node to be removed   
+    //set previous.next to removed.next
+    //decrement length
+    //return the removed node
+
+    if(index<0 || index>this.length-1) return false;
+    if(index===0) return !!this.shift();
+    if(index===this.length-1) return !!this.pop();
+    let previous = this.get(index-1);
+    // let removed =this.get(index);
+    let removed = previous.next;
+    previous.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+  //prints all values in the list in order
+  print(){
+    let current = this.head;
+    while(current){
+      console.log(current.value);
+      current = current.next;
+    }
+    return this.length;
+  }
+  //reverse the list
+  reverse(){
+    // get oldhead in temp
+    //set head = this.tail
+    //set tail = temp
+    //set previous = head
+   // while(current!==tail)
+    // set current = previous.next
+    //set next = current.next
+    //set previous.next = current
+
+    var node = this.head;
+    this.head=this.tail;
+    this.tail = node;
+
+    let previous = null;
+    let next=null;
+
+    for(let i=0;i<this.length;i++){
+      next = node.next;
+      node.next=previous;
+      previous=node;
+      node=next;
+    }
+    return this;
   }
 }
 
@@ -155,11 +247,24 @@ let list = new singly_linked_list();
 list.push("1");
 list.push("3");
 list.push("5");
+list.push("LAST");
 // console.log("popped node 1",list.pop());
 // console.log("popped node 2",list.pop());
 // console.log("popped node 3",list.pop());
 // console.log("popped node lasttry",list.pop());
 console.log("shifted node ", list.shift());
 console.log("unshifted node ", list.unshift("7"));
-console.log("getting node 1 value ",list.get(1));
-console.log(list);
+console.log("lsit",list.print());
+console.log("getting node 1 value ", list.get(1));
+console.log("setting node 2 value", list.set(2, "two"));
+console.log("setting node 10 value", list.set(10, "ten"));
+console.log("lsit",list.print());
+console.log("insert new node at 1",list.insert(1,"one"));
+console.log("lsit",list.print());
+console.log("removing last node",list.remove(5));
+console.log("removing last node",list.remove(4));
+// console.log(list);
+console.log("list length:",list.print());
+console.log("reversing ",list.reverse());
+console.log("lsit",list.print());
+
