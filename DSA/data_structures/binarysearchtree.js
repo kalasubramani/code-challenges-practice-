a4393efd11326c150d745f947a50f68e7f8f24f7
KalaSubramani <kalasubramani@gmail.com>
2024-07-31 13:13:09 -0400

    binary search tree - fixed find()

--- DSA/data_structures/binarysearchtree.js
+++ DSA/data_structures/binarysearchtree.js
@@ -14,22 +14,20 @@ class binarysearchtree {
   //insert a new node to a bst
   insert(value) {
     //create new node with provided value
-    //check if root exists
-    // else - make new node as root, return Tree
-    //check if value > root   
-    //while value > root       
-    //let new root = root.right
-    // if there is no root.right
-    //  set root.right = new node
-    // else if root < value
-    //set root.left = new node
-    //else
-    // while value < root
-    // let new root = root.left
-    //if there is no root.left
-    //set new node as root.left
-    //else if value > root
-    //set root.right = new node
+    //if root does not exists, make new node as root, return Tree
+       
+    //while true 
+      //if root.value === value, then return undefined
+      //check if value > root  
+          // if there is no root.right
+                //  set root.right = new node
+                //return tree
+          //else new root = root.right and continue
+      // else if root < value
+        //if there is no root.left
+              //set new node as root.left
+              //return tree
+        //else new root = root.left and continue
 
     let newNode = new Node(value);
     if (!this.root) {
@@ -59,26 +57,28 @@ class binarysearchtree {
 
   //find if a value exists in given tree
   find(value){
-    //if there is no root, return false
-    
-    //while true   
-      //check if root == value then return root
-       //check if value > root                   
-           //let new root = root.right
-           // if root.right ==value
-                //  return root.right
+    //if there is no root, return false  
+     //check if root === value then return root  
+    //while true        
+       //check if value > root 
+           //if there is no right node, return false   
+           // if root.right ===value then return true
              //set root = root.right
          //else
+           //if there is no left node, return false
+           //if root.left === value, then return true
            //set root as root.left
-           //else if value > root
-    //set root.right = new node
+           
+    //return false
 
     if(!this.root)  return undefined;
-    if(this.root.value=value) return true;//return this.root;
-
+    if(this.root.value===value) return true;//return this.root;
+    
     let rootNode = this.root,hasValue=false;
+    console.log("finding..",value)
+    // console.log(rootNode.right.value,rootNode.left.value,value) 
     while(rootNode && !hasValue){
-      if(value > rootNode.value){        
+      if(value > rootNode.value){              
         if(!rootNode.right) return hasValue;
         else if(rootNode.right.value===value) return !hasValue;// rootNode.right;
         rootNode=rootNode.right;        
@@ -86,11 +86,9 @@ class binarysearchtree {
         if(!rootNode.left) return hasValue;
         else if(rootNode.left.value===value) return !hasValue;//rootNode.left;
         rootNode=rootNode.left;
-      }
-      return found;
+      }     
     }
-    
-
+    return hasValue;
   }
 }
 
@@ -99,6 +97,7 @@ bst.root = new Node(10);
 bst.root.left = new Node(8);
 bst.root.right = new Node(15);
 bst.insert(9);
-// console.log(bst);
-console.log(bst.find(8));
+console.log(bst);
+//  console.log(bst.find(10));
 console.log(bst.find(99));
+console.log(bst.find(15));
