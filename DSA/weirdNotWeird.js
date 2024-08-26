35c7e294c77f250682e098988e563f1ee7604f28
KalaSubramani <kalasubramani@gmail.com>
2024-08-26 11:46:51 -0400

    Maxx - Study Lab - Bonus challenge

--- DSA/weirdNotWeird.js
+++ DSA/weirdNotWeird.js
@@ -1,24 +1,31 @@
 
-function checkRange(n){  
-   if(n>=6 && n<=20)
-    return true;
-  else //if(n>20) or (n>=2 && n<=5)
-    return false;
+function isEvenWithinRange(n) {
+  if (n >= 6 && n <= 20) return true;
+  return false;//n>20 or (n>=2 && n<=5)   
 }
 
-function weirdNotWeird(n){
-  if(!Number.isInteger(n) || n>100) console.log("Invalid input!");;
-  
-  //if n is odd, print weird
-  if(n%2 !== 0){
-    return true;
-  }else{
-    //check range for even nums and print accordingly
-      checkRange(n);
+function isOdd(n){
+  return (n % 2 !== 0) ;
+}
+
+function weirdNotWeird(input) {
+  //base cnd
+  if (!input.length) return;
+
+  //call the function recursively for each ele in the array
+  let current = input.pop();
+  weirdNotWeird(input);
+
+  //if n is odd, print weird else check range for even nums
+  let isWeird = false;
+  if (!Number.isInteger(current) || current===0 || current > 100) {
+    console.log("Invalid");
+  } else {   
+    isWeird = isOdd(current)|| isEvenWithinRange(current);
+    isWeird ? console.log("Weird") : console.log("Not Weird")
   }
 }
 
 
-
-weirdNotWeird(3)?console.log("Weird"):console.log("Not Weird");
-weirdNotWeird(24)?console.log("Weird"):console.log("Not Weird");
+let input = [123, 12312, 3, 2, 5, 7, 21, 24,0,4]
+weirdNotWeird(input);
