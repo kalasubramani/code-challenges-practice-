e713354c21e984e4872d19883ad134c868a6d439
KalaSubramani <kalasubramani@gmail.com>
2024-11-08 16:44:04 -0500

    Maxx - coding challege - find all anagrams of given string

--- Maxx/allAnagrams.js
+++ Maxx/allAnagrams.js
@@ -37,7 +37,7 @@ function allAnagrams(str){
      rotate++;
    }
    
- return anagramSet;
+ return [...anagramSet];
 }
 
 function getAllAnagrams(str,anagramSet,n){
@@ -47,60 +47,4 @@ function getAllAnagrams(str,anagramSet,n){
   }
 }
 
-// console.log(allAnagrams('cat'));
-
-//bonus challenge #1
-// #1 Include strings that can be made with less letters than the input.
-//  Constraint: No one letter words
-// Ex. in: cat 
-// out: ['cat', 'cta', 'act', 'atc', 'tac', 'tca', 'ca', 'ct', 'ta', 'tc', 'ac', 'at']
-
-function allSubstrings(str){
-  //define a set to hold results - elimates duplicates at insertion
-  //iterate thru chars in string
-   //get char at 1 + letters before i + letter after i
-   //push all combinations of substrings to substringSet
-   
-   //if end of string is reached, rotate the string to right by 1 
-   //incr rotation by 1
-
-   //if rotations >= string length
-   //break
-   //else continue
-
-   //return substringSet
-
-   let substringSet = new Set(),n=str.length,rotation=0;
-   
-  while(rotation<n){
-    getAllSubstrings(str,substringSet,n);
-    let roateright = str.slice(1).concat(str.slice(0,1));
-    console.log("roateright",roateright);
-    str=roateright;
-    rotation++;
-  }
-  // rotation=0;
-  // while(rotation<n){
-  //   let rotateleft= str.slice(n-1).concat(str.slice(0,n-1));
-  //   console.log("rotateleft",rotateleft);
-  //   getAllSubstrings(rotateleft,substringSet,n)
-  //   str=rotateleft;
-  //   rotation++;
-  // }
-  return substringSet;
-}
-
-function getAllSubstrings(str,substringSet,n){
-  console.log("ip",str)
-  for(let i=0;i<n ;i++){//cat ca   at  tc   ta ct ac
-    for(let j=i+1;j<n;j++){
-      console.log(str[i], str[j])
-      substringSet.add(str[i]+ str[j]);
-      // console.log(str.slice(i,j))
-
-      // substringSet.add(str.slice(i,j))
-      // anagramSet.add(str.slice(i,i+1).concat(str.slice(0,i)).concat(str.slice(i+1)));
-    }   
-   }
-}
-console.log(allSubstrings("cat"));
+console.log(allAnagrams('cat'));
